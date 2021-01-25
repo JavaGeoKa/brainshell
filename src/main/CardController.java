7bfd0bb316f24b465822687e92a9d3614dfadc6c
g <MDf4nBNjqrufGnxS@protonmail.com>
2021-01-25 14:21:05 +0300

    v2

--- src/main/CardController.java
+++ src/main/CardController.java
@@ -10,22 +10,13 @@ import java.util.*;
 
 public class CardController {
 
-    //ControllerSettings
     static int y = 591;
     static int[] positions = {149, 220, 292, 364, 436};
     static int cardPlace = 0;
-
-
-    //card collection
     static int countFiles =0;
-    private static final String DIR_PATH_RED = "/home/g/IdeaProjects/CardRecognizer/cards/red";
-    private static final String DIR_PATH_BLACK = "/home/g/IdeaProjects/CardRecognizer/cards/black";
 
-    //input file
-//    private static final String PATH = "/home/g/Downloads/java_test_task/imgs/20180821_094458.109_0x1FE201D8.png";
     private static final String DIR_PATH = "/home/g/Downloads/java_test_task/imgs/";
     private static List<String> patchFiles = new ArrayList<>();
-
 
     static Map<BufferedImage, String> currentCards = new LinkedHashMap<>();
     static Map<BufferedImage, String> redCards = new HashMap<>();
@@ -36,12 +27,8 @@ public class CardController {
     static BufferedImage subImg;
     static StringBuilder answer = new StringBuilder();
 
-
-
     public static void main(String[] args) throws IOException {
-
-        //загружаем коллекцию красных карт
-        Files.walk(Paths.get(DIR_PATH_RED))
+        Files.walk(Paths.get(new File("").getAbsolutePath()+"/cards/red/"))
                 .filter(Files::isRegularFile)
                 .forEach(i -> {
                     try {
@@ -50,10 +37,7 @@ public class CardController {
                         e.printStackTrace();
                     }
                 });
-        System.out.println("Red cards loaded: " + redCards.size());
-
-        //загружаем коллекцию черных карт
-        Files.walk(Paths.get(DIR_PATH_BLACK))
+        Files.walk(Paths.get(new File("").getAbsolutePath()+"/cards/black/"))
                 .filter(Files::isRegularFile)
                 .forEach(i -> {
                     try {
@@ -62,17 +46,13 @@ public class CardController {
                         e.printStackTrace();
                     }
                 });
-        System.out.println("Black cards loaded: " + blackCards.size());
-
-        //Загружаем ссылки на скриншоты
-        Files.walk(Paths.get(DIR_PATH))
+        Files.walk(Paths.get(args[0]))
                 .filter(Files::isRegularFile)
                 .forEach(i -> {
                     patchFiles.add(i.toString());
                     countFiles++;
                 });
         System.out.println("Files to handle: " + countFiles);
-
 
         patchFiles.forEach(p -> {
             try {
@@ -81,43 +61,28 @@ public class CardController {
                 e.printStackTrace();
             }
         });
-
-
     }
-
-
-//    обработка одного файла
     private static String linkHandle(String p) throws IOException {
-
-        answer.setLength(0);
-        //загрyжаем скриншот экрана
         img = ImageIO.read(new File(p));
-
-        //вырезаю карты
+        answer.setLength(0);
+        currentCards.clear();
         for (cardPlace = 0; cardPlace < 5; cardPlace++) {
             subImg = img.getSubimage(positions[cardPlace], y,29, 45);
             if (checkIsItCard(subImg)) {
                 currentCards.put(subImg, checkColor(subImg));
             }
         }
-//        cardPlace = 0;
-
-        //Здесь нужно начать сравнивать два изображения
         currentCards.entrySet().stream().forEach( e -> {
             if (e.getValue() == "black") {
-                //black compare
                 answer.append(blackCompare(e.getKey()));
             } else if (e.getValue() == "red") {
-                //red compare возвращает название карты с минимальной разницей
                 answer.append(redCompare(e.getKey()));
             }
         });
-
-
+        currentCards.clear();
         return p + " -> " + answer;
     }
 
-    //сравниваю изображение с черными картами
     private static String blackCompare(BufferedImage key) {
         differences.clear();
         //добавить разницу в мепу
@@ -133,11 +98,8 @@ public class CardController {
 
     }
 
-
-    //сравниваю изображение с красными картами
     private static String redCompare(BufferedImage key) {
         differences.clear();
-        //добавить разницу в мепу
         redCards.entrySet().stream().forEach(rc -> {
             try {
                 differences.put(CardComparator.compare(rc.getKey(),key), rc.getValue());
@@ -145,23 +107,19 @@ public class CardController {
                 e.printStackTrace();
             }
         });
-        //возвращаем название карты
         return differences.firstEntry().getValue();
 
     }
 
-    //определение цвета карты
     private static String checkColor(BufferedImage i) {
         if (i.getRGB(10,35) == -14474458) {
             return "black";
         } else if (i.getRGB(10,35) == -3323575) {
             return "red";
         }
-
         return "red";
     }
 
-    //проверка карта или нет
     private static boolean checkIsItCard(BufferedImage i) {
         int backgroundQuantities = 0;
         for (int l = 0; l < i.getWidth(); l++) {
@@ -175,7 +133,6 @@ public class CardController {
         return false;
     }
 
-    //убираю тень
     private static void backgroundChanger(BufferedImage i) {
         for (int k = 0; k < i.getHeight(); k++) {
             for (int l = 0; l < i.getWidth(); l++) {
@@ -190,6 +147,4 @@ public class CardController {
             }
         }
     }
-
-
 }
